12d106f4648ab7290324871645b1d2c74f39dcc0
Brenno Oliveira <BrennoOliveirq@outlook.com>
2025-08-01 19:20:34 -0300

    correção de bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
-import type React from "react"
-import type { Metadata } from "next"
-import "./globals.css"
-import { ProjectProvider } from "@/lib/project-context"
-import { AuthProvider } from "@/components/admin/auth-context"
-import { Toaster } from "@/components/ui/toaster"
+import type React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
+import { ProjectProvider } from "@/lib/project-context";
+import { AuthProvider } from "@/components/admin/auth-context";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "DevDuo",
   description: "Transforme sua presença digital com Landing Pages extraordinárias",
-  
-}
+  icons: {
+    icon: "/favicon-32x32.png", // <- favicon
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="pt-BR">
@@ -27,5 +29,5 @@ export default function RootLayout({
         </AuthProvider>
       </body>
     </html>
-  )
+  );
 }
